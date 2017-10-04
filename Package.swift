e038d09191211eba5c4babee7b1d0e3541d66f4f
Jonathan Guthrie <jono@perfect.org>
2017-10-04 15:32:59 -0400

    update for swift 4/perfect 3

--- Package.swift
+++ Package.swift
@@ -1,14 +1,14 @@
+// Generated automatically by Perfect Assistant Application
+// Date: 2017-10-04 19:25:51 +0000
 import PackageDescription
-
 let package = Package(
-    name: "OAuth2",
-    targets: [],
-    dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 1),
+	name: "OAuth2",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 3),
 		.Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 2),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", majorVersion: 1)
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", majorVersion: 3),
 	]
-
 )
